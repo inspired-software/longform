6a50ab8a48a0f2cc3671d80efc0eba960ba53f52
Eric Summers <eric_summers@icloud.com>
2021-12-19 21:38:16 -0500

    Add example app project.

---
+++ Longform Examples/Longform Examples/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Longform_ExamplesApp.swift
+//  Longform Examples
+//
+//  Created by Eric Summers on 12/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
